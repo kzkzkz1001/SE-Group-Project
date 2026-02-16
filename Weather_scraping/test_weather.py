70b5dbfd8facd419562ed43cbb9d9aa65ed7e413
joannasaheed <joanna.saheed@ucdconnect.ie>
2026-02-16 13:28:05 +0000

    Delete Weather_scraping directory

--- Weather_scraping/test_weather.py
+++
@@ -1,8 +0,0 @@
-# test_weather.py
-from weather_scraper import scrape_weather
-
-if __name__ == "__main__":
-    print("Testing single weather scrape...")
-    scrape_weather()
-    print("Test complete! Check your database.")
-    
